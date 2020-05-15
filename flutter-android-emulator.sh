38c31b12ac90cbdad0d0dcd0aea55279e9f7efa6
Mats Pfeiffer <mats.pfeiffer@googlemail.com>
2020-05-15 22:09:49 +0200

    Increase waiting time for emulator

--- flutter-android-emulator.sh
+++ flutter-android-emulator.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 flutter emulators --launch pixel_9.0
-sleep 10
+sleep 30
 flutter run -d $FLUTTER_EMULATOR_NAME --observatory-port $FLUTTER_DEBUG_PORT
 /bin/bash /usr/local/bin/chown.sh
 
